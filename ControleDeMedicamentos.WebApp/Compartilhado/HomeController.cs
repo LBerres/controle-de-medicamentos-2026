0ecf57024db21290f6c42043043e7736d7cb5ab0
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-07-22 22:56:02 -0300

    Renomeado ConsoleApp para WebApp / Move HomeController / Adicionado Listagem de Fornecedores

---
+++ ControleDeMedicamentos.WebApp/Compartilhado/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeMedicamentos.WebApp.Compartilhado;
+
+public class HomeController : Controller
+{
+    // Ações= Métodos
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
