cb91eec2a1394116a28db146e3ab7b69fbeb4ff8
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-07-24 21:12:50 -0300

    Corrige Fornecedores Faltando Durante Cadastro de Medicamentos

--- ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
+++ ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
@@ -1,4 +1,5 @@
 using ControleDeMedicamentos.WebApp.Compartilhado.Arquivos;
+using ControleDeMedicamentos.WebApp.ModuloFornecedores;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeMedicamentos.WebApp.ModuloMedicamentos;
@@ -6,7 +7,8 @@ namespace ControleDeMedicamentos.WebApp.ModuloMedicamentos;
 public sealed class MedicamentoController : Controller
 {
 
-    private readonly RepositorioMedicamentoEmArquivo repositorio;
+    private readonly RepositorioMedicamentoEmArquivo repositorioMedicamento;
+    private readonly RepositorioFornecedorEmArquivo repositorioFornecedor;
 
     public MedicamentoController()
     {
@@ -14,13 +16,37 @@ public sealed class MedicamentoController : Controller
 
         contexto.Carregar();
 
-        repositorio = new RepositorioMedicamentoEmArquivo(contexto);
+        repositorioMedicamento = new RepositorioMedicamentoEmArquivo(contexto);
+        repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
     }
 
     [HttpGet]
     public ActionResult Listar()
     {
-        List<Medicamento> medicamentos = repositorio.SelecionarTodos();
+        List<Medicamento> medicamentos = repositorioMedicamento.SelecionarTodos();
         return View(medicamentos);
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        List<Fornecedor> fornecedores = repositorioFornecedor.SelecionarTodos();
+
+        ViewBag.Fornecedores = fornecedores;
+
+        return View();
+    }
+    [HttpPost]
+    public ActionResult Cadastrar(string nome, string descricao, int fornecedorId)
+    {
+        Fornecedor? fornecedor = repositorioFornecedor.SelecionarPorId(fornecedorId);
+        if (fornecedor == null)
+            return NotFound();
+
+        Medicamento medicamento = new Medicamento(nome, descricao, fornecedor);
+
+        repositorioMedicamento.Cadastrar(medicamento);
+
+        return RedirectToAction(nameof(Listar));
     }
 }
